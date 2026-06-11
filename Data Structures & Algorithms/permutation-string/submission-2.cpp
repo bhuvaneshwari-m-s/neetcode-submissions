c805e422680545a1143f1b701008e22d155bf400
bhuvaneshwari-m-s <bhuvaneshwarimuttannasilli.231ec114@nitk.edu.in>
2026-06-11 07:13:57 +0530

    Add: permutation-string - submission-2

---
+++ Data Structures & Algorithms/permutation-string/submission-2.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        vector<int> freq1(26, 0), freq2(26, 0);
+
+        for(char ch : s1)
+            freq1[ch-'a']++;
+
+        for(int i = 0 ; i < s2.length() ; i++)
+        {
+            vector<int> freq2(26, 0);
+            for(int j = i ; j < s2.length() ; j++)
+            {
+                freq2[s2[j]-'a']++;
+                if(freq2[s2[j]-'a'] > freq1[s2[j]-'a'])
+                    break;
+                bool flag = true;
+                for(int i = 0 ; i < 26 ; i++)
+                {
+                    if(freq1[i] != freq2[i])
+                    {
+                        flag = false;
+                        break;
+                    }
+                }
+                if(flag)
+                    return true;
+            }
+        }
+
+        return false;
+    }
+};
