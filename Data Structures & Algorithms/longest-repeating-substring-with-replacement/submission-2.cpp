1f1f099f09a93b398093fdf951b29247ac734e80
bhuvaneshwari-m-s <bhuvaneshwarimuttannasilli.231ec114@nitk.edu.in>
2026-06-11 06:50:26 +0530

    Add: longest-repeating-substring-with-replacement - submission-2

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-2.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        int res = 0;
+        unordered_set<char> set(s.begin(), s.end());
+        for(char ch : set)
+        {
+            int count = 0;
+            int left = 0;
+            for(int right = 0 ; right < s.length(); right++)
+            {
+                if(s[right] == ch)
+                    count++;
+                
+                while(right - left + 1 - count > k)
+                {
+                    if(s[left] == ch)
+                        count--;
+                    left++;
+                }
+                res = max(res, right - left + 1);
+            }
+        }
+        return res;
+        
+    }
+};
