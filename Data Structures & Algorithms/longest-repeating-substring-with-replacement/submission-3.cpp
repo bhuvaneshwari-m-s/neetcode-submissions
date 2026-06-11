481ae774c7622b0dac4655b836ff6a42c655381a
bhuvaneshwari-m-s <bhuvaneshwarimuttannasilli.231ec114@nitk.edu.in>
2026-06-11 06:53:56 +0530

    Add: longest-repeating-substring-with-replacement - submission-3

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-3.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        int l = 0;
+        int res = 0;
+        vector<int> freq(26, 0);
+        int maxFreq = 0;
+        for(int r = 0 ; r < s.length() ; r++)
+        {
+            freq[s[r]-'A']++;
+            maxFreq = max(maxFreq , freq[s[r]-'A']);
+            
+            while(r-l+1 - maxFreq > k)
+            {
+                freq[s[l] - 'A']--;
+                l++;
+            }
+            res = max(res, r - l + 1);
+        }
+        return res;
+    }
+};
